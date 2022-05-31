8f7c8c4879084ebf630175c8570fdb8279aa430b
Bouna Mohamed Mahfoudh <83521427+medmahfoudh@users.noreply.github.com>
2022-05-31 10:30:03 +0100

    Add files via upload

---
+++ Sonar Datasets Machine Learning.R
@@ -0,0 +1,87 @@
+library(class) 
+library(caret) 
+require(mlbench) 
+library(e1071) 
+library(base) 
+require(base)
+
+data(Sonar)
+head(Sonar)
+
+cat("number of rows and columns are:", nrow(Sonar), ncol(Sonar))
+
+base::table(Sonar$Class)
+apply(Sonar, 2, function(x) sum(is.na(x)))
+
+
+
+SEED <- 123
+set.seed(SEED) 
+data <- Sonar[base::sample(nrow(Sonar)), ] 
+bound <- floor(0.7 * nrow(data)) 
+df_train <- data[1:bound, ] 
+df_test <- data[(bound + 1):nrow(data), ] 
+cat("number of training and test samples are ", nrow(df_train), nrow(df_test))
+cat("number of training classes: \n", base::table(df_train$Class)/ nrow(df_train))
+cat("\n") 
+cat("number of test classes: \n", base::table(df_test$Class)/nrow(df_test)) 
+
+
+#Pour simplifier notre travail, nous pouvons créer les data frame suivants
+
+X_train <- subset(df_train, select=-Class)
+y_train <- df_train$Class 
+X_test <- subset(df_test, select=-Class) # exclude Class for prediction 
+y_test <- df_test$Class
+
+
+# Entraînement d'un modèle sur des données
+
+model_knn <- knn(train=X_train,
+                 test=X_test, 
+                 cl=y_train, # class labels 
+                 k=3) 
+model_knn 
+
+
+#Évaluation de la performance du modèle
+conf_mat <- base::table(y_test, model_knn)
+conf_mat 
+
+#Pour calculer la précision, nous additionnons toutes les observations correctement classées (situées 
+#dans la diagonale) et les divisons par le nombre total de classes
+
+cat("Test accuracy: ", sum(diag(conf_mat))/sum(conf_mat))
+
+knn_loocv <- knn.cv(train=X_train, cl=y_train, k=3)
+knn_loocv
+
+#n pour calculer la précision des étiquettes d'apprentissage y_train et 
+#des prédictions validées par recoupement knn_loocv
+
+conf_mat_cv <- base::table(y_train, knn_loocv)
+conf_mat_cv 
+cat("LOOCV accuracy: ", sum(diag(conf_mat_cv)) / sum(conf_mat_cv)) 
+
+#Améliorer les performances du modèle
+
+SEED <- 2016
+set.seed(SEED) 
+# Créer L'entrainement au 70% de datasets. 
+in_train <- createDataPartition(Sonar$Class, p=0.7, list=FALSE) 
+ndf_train <- Sonar[in_train, ] 
+ndf_test <- Sonar[-in_train, ] 
+
+
+ctrl <- trainControl(method="repeatedcv", number=5, repeats=2) 
+nn_grid <- expand.grid(k=c(1,3,5,7)) 
+nn_grid
+
+
+set.seed(SEED)
+best_knn <- train(Class~., data=ndf_train, 
+                  method="knn", 
+                  trControl=ctrl, 
+                  preProcess = c("center", "scale"), # standardize 
+                  tuneGrid=nn_grid) 
+best_knn
